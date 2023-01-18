1b7f00c556f6b9718b7a706b63dbf50c9dd4fbec
xzjisme <xzhengjia@seu.edu.cn>
2023-01-18 16:57:02 +0800

    new playground strategy brings better res

--- playground.py
+++ playground.py
@@ -25,9 +25,23 @@ if __name__ == "__main__":
     for i in range(len(text)):
         if text[i] == "的" or text[i] == "地" or text[i] == "得":
             opt = None
-            for j in range(10):
+            for j in range(5):
                 l_ = random.randint(1, 10)
                 r_ = random.randint(1, 10)
+                sample = (text[i - l_ : i], text[i + 1 : i + r_], dict_dedede[text[i]])
+                tokenized_sample = tokenizer(sample[0], sample[1], padding="longest")
+                for key in tokenized_sample.keys():
+                    tokenized_sample[key] = [tokenized_sample[key]]
+                for key in tokenized_sample.keys():
+                    tokenized_sample[key] = torch.tensor(tokenized_sample[key])
+                output = model(**tokenized_sample)
+                if opt is None:
+                    opt = output
+                else:
+                    opt += output
+            for j in range(5):
+                l_ = 10
+                r_ = 10
                 sample = (text[i - l_ : i], text[i + 1 : i + r_], dict_dedede[text[i]])
                 tokenized_sample = tokenizer(sample[0], sample[1], padding="longest")
                 for key in tokenized_sample.keys():
